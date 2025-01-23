04d4aef05b310ea5d05e74af3ff430cd755cddac
Suman Adhikari <expertxkid@gmail.com>
2025-01-23 15:51:21 -0600

    feat/Chapter03: implement ProductService in product-service

--- Chapter03/2-basic-rest-services/microservices/product-service/src/main/java/np/com/suman_adhikari/microservices/core/product/ProductServiceApplication.java
+++ Chapter03/2-basic-rest-services/microservices/product-service/src/main/java/np/com/suman_adhikari/microservices/core/product/ProductServiceApplication.java
@@ -2,7 +2,9 @@ package np.com.suman_adhikari.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "np.com.suman_adhikari")
 @SpringBootApplication
 public class ProductServiceApplication {
 
