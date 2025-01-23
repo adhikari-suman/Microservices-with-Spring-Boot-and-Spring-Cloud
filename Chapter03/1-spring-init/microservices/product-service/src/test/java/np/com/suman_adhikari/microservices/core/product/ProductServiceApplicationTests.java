cd6a9f224f47506163c2d5d12c8945333ac5a639
Suman Adhikari <expertxkid@gmail.com>
2025-01-23 11:56:53 -0600

    init/chapter03: init project with multiple stage build

---
+++ Chapter03/1-spring-init/microservices/product-service/src/test/java/np/com/suman_adhikari/microservices/core/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.suman_adhikari.microservices.core.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Chapter03/1-spring-init/microservices/review-service/src/main/java/np/com/suman_adhikari/microservices/core/review/ReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package np.com.suman_adhikari.microservices.core.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServiceApplication.class, args);
+	}
+
+}
