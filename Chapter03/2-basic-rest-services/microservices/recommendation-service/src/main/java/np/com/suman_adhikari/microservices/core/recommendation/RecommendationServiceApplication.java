6dbc1402a0ad67caad96de83dedb7b6b5b172ea6
Suman Adhikari <expertxkid@gmail.com>
2025-01-23 12:12:19 -0600

    init/Chapter03: init starter files for 2-basic-rest-services

---
+++ Chapter03/2-basic-rest-services/microservices/recommendation-service/src/main/java/np/com/suman_adhikari/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package np.com.suman_adhikari.microservices.core.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationServiceApplication.class, args);
+	}
+
+}
