2a62acc7046a3e8c52322f6563b4066b8e0b2efe
Suman Adhikari <expertxkid@gmail.com>
2025-01-25 11:40:53 -0600

    init/Chapter04: init project

---
+++ Chapter04/api/src/main/java/np/com/suman_adhikari/api/exceptions/NotFoundException.java
@@ -0,0 +1,18 @@
+package np.com.suman_adhikari.api.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException() {}
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+}
---
+++ Chapter04/microservices/recommendation-service/src/main/java/np/com/suman_adhikari/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -0,0 +1,15 @@
+package np.com.suman_adhikari.microservices.core.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("np.com.suman_adhikari")
+@SpringBootApplication
+public class RecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationServiceApplication.class, args);
+	}
+
+}
