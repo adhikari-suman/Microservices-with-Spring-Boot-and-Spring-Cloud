138e444b7e5cbcb179cdad3467518af1a735dbe1
Suman Adhikari <expertxkid@gmail.com>
2025-01-25 11:05:03 -0600

    feat/Chapter03: implement review-service and write unit tests

--- Chapter03/2-basic-rest-services/microservices/review-service/src/main/java/np/com/suman_adhikari/microservices/core/review/ReviewServiceApplication.java
+++ Chapter03/2-basic-rest-services/microservices/review-service/src/main/java/np/com/suman_adhikari/microservices/core/review/ReviewServiceApplication.java
@@ -2,7 +2,9 @@ package np.com.suman_adhikari.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("np.com.suman_adhikari")
 @SpringBootApplication
 public class ReviewServiceApplication {
 
