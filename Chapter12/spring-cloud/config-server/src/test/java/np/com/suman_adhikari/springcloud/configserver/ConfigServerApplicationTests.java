c46271ad67aa0293661dd9f63ba012c50ef513b4
Suman Adhikari <expertxkid@gmail.com>
2025-01-31 15:54:42 -0600

    feat/Chapter12: add config-server

--- Chapter12/spring-cloud/config-server/src/test/java/np/com/suman_adhikari/springcloud/configserver/ConfigServerApplicationTests.java
+++ Chapter12/spring-cloud/config-server/src/test/java/np/com/suman_adhikari/springcloud/configserver/ConfigServerApplicationTests.java
@@ -3,7 +3,12 @@ package np.com.suman_adhikari.springcloud.configserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"spring.profiles.active=native"})
 class ConfigServerApplicationTests {
 
 	@Test
