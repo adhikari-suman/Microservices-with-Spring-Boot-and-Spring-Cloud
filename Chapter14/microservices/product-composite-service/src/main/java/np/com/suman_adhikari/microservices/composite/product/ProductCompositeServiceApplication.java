146b87e80e44e840a382d1a627e1eccbc2445143
Suman Adhikari <expertxkid@gmail.com>
2025-02-04 12:06:54 -0600

    feat/Chapter14: add micrometer and zipkin reporter to microservices

--- Chapter14/microservices/product-composite-service/src/main/java/np/com/suman_adhikari/microservices/composite/product/ProductCompositeServiceApplication.java
+++ Chapter14/microservices/product-composite-service/src/main/java/np/com/suman_adhikari/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -15,16 +15,35 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Hooks;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
-@ComponentScan("np.com.suman_adhikari")
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Hooks;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
 @SpringBootApplication
+@ComponentScan("np.com.suman_adhikari")
 public class ProductCompositeServiceApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProductCompositeServiceApplication.class);
-    private final Integer threadPoolSize;
-    private final Integer taskQueueSize;
+
     @Value("${api.common.version}")         String apiVersion;
     @Value("${api.common.title}")           String apiTitle;
     @Value("${api.common.description}")     String apiDescription;
@@ -36,18 +55,6 @@ public class ProductCompositeServiceApplication {
     @Value("${api.common.contact.name}")    String apiContactName;
     @Value("${api.common.contact.url}")     String apiContactUrl;
     @Value("${api.common.contact.email}")   String apiContactEmail;
-    @Autowired
-    public ProductCompositeServiceApplication(
-            @Value("${app.threadPoolSize:10}") Integer threadPoolSize,
-            @Value("${app.taskQueueSize:100}") Integer taskQueueSize
-                                             ) {
-        this.threadPoolSize = threadPoolSize;
-        this.taskQueueSize  = taskQueueSize;
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(ProductCompositeServiceApplication.class, args);
-    }
 
     /**
      * Will exposed on $HOST:$PORT/swagger-ui.html
@@ -73,16 +80,35 @@ public class ProductCompositeServiceApplication {
                                       .url(apiExternalDocUrl));
     }
 
+    private final Integer threadPoolSize;
+    private final Integer taskQueueSize;
+
+    @Autowired
+    public ProductCompositeServiceApplication(
+            @Value("${app.threadPoolSize:10}") Integer threadPoolSize,
+            @Value("${app.taskQueueSize:100}") Integer taskQueueSize
+                                             ) {
+        this.threadPoolSize = threadPoolSize;
+        this.taskQueueSize = taskQueueSize;
+    }
+
     @Bean
     public Scheduler publishEventScheduler() {
         LOG.info("Creates a messagingScheduler with connectionPoolSize = {}", threadPoolSize);
         return Schedulers.newBoundedElastic(threadPoolSize, taskQueueSize, "publish-pool");
     }
 
+    @Autowired
+    private ReactorLoadBalancerExchangeFilterFunction lbFunction;
+
     @Bean
-    @LoadBalanced
-    public WebClient.Builder loadBalancedWebClientBuilder() {
-        return WebClient.builder();
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.filter(lbFunction).build();
+    }
+
+    public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
+        SpringApplication.run(ProductCompositeServiceApplication.class, args);
     }
 
 }
--- Chapter14/microservices/product-composite-service/src/main/java/np/com/suman_adhikari/microservices/composite/product/services/ProductCompositeIntegration.java
+++ Chapter14/microservices/product-composite-service/src/main/java/np/com/suman_adhikari/microservices/composite/product/services/ProductCompositeIntegration.java
@@ -10,8 +10,10 @@ import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
 import java.io.IOException;
 import java.net.URI;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,16 +42,18 @@ import np.com.suman_adhikari.util.http.HttpErrorInfo;
 import np.com.suman_adhikari.util.http.ServiceUtil;
 
 @Component
-public class ProductCompositeIntegration implements ProductService, RecommendationService, ReviewService {
+public class ProductCompositeIntegration implements ProductService,
+                                                    RecommendationService,
+                                                    ReviewService {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProductCompositeIntegration.class);
 
-    private static final String PRODUCT_SERVICE_URL = "http://product";
+    private static final String PRODUCT_SERVICE_URL        = "http://product";
     private static final String RECOMMENDATION_SERVICE_URL = "http://recommendation";
-    private static final String REVIEW_SERVICE_URL = "http://review";
+    private static final String REVIEW_SERVICE_URL         = "http://review";
 
-    private final Scheduler publishEventScheduler;
-    private final WebClient webClient;
+    private final Scheduler    publishEventScheduler;
+    private final WebClient    webClient;
     private final ObjectMapper mapper;
     private final StreamBridge streamBridge;
 
@@ -58,17 +62,17 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
     @Autowired
     public ProductCompositeIntegration(
             @Qualifier("publishEventScheduler") Scheduler publishEventScheduler,
-            WebClient.Builder webClientBuilder,
+            WebClient webClient,
             ObjectMapper mapper,
             StreamBridge streamBridge,
             ServiceUtil serviceUtil
                                       ) {
-        this.webClient = webClientBuilder.build();
+        this.webClient = webClient;
 
         this.publishEventScheduler = publishEventScheduler;
-        this.mapper = mapper;
-        this.streamBridge = streamBridge;
-        this.serviceUtil = serviceUtil;
+        this.mapper                = mapper;
+        this.streamBridge          = streamBridge;
+        this.serviceUtil           = serviceUtil;
     }
 
     @Override
@@ -86,19 +90,27 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
     @CircuitBreaker(name = "product", fallbackMethod = "getProductFallbackValue")
     public Mono<Product> getProduct(int productId, int delay, int faultPercent) {
 
-        URI url = UriComponentsBuilder.fromUriString(PRODUCT_SERVICE_URL
-                                                     + "/product/{productId}?delay={delay}&faultPercent={faultPercent}").build(productId, delay, faultPercent);
+        URI url = UriComponentsBuilder.fromUriString(
+                                              PRODUCT_SERVICE_URL + "/product/{productId}?delay={delay}&faultPercent={faultPercent}")
+                                      .build(productId, delay, faultPercent);
         LOG.debug("Will call the getProduct API on URL: {}", url);
 
-        return webClient.get().uri(url)
-                        .retrieve().bodyToMono(Product.class).log(LOG.getName(), FINE)
+        return webClient.get()
+                        .uri(url)
+                        .retrieve()
+                        .bodyToMono(Product.class)
+                        .log(LOG.getName(), FINE)
                         .onErrorMap(WebClientResponseException.class, ex -> handleException(ex));
     }
 
-    private Mono<Product> getProductFallbackValue(int productId, int delay, int faultPercent, CallNotPermittedException ex) {
+    private Mono<Product> getProductFallbackValue(
+            int productId, int delay, int faultPercent, CallNotPermittedException ex
+                                                 ) {
 
-        LOG.warn("Creating a fail-fast fallback product for productId = {}, delay = {}, faultPercent = {} and exception = {} ",
-                 productId, delay, faultPercent, ex.toString());
+        LOG.warn(
+                "Creating a fail-fast fallback product for productId = {}, delay = {}, faultPercent = {} and exception = {} ",
+                productId, delay, faultPercent, ex.toString()
+                );
 
         if (productId == 13) {
             String errMsg = "Product Id: " + productId + " not found in fallback cache!";
@@ -106,14 +118,16 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
             throw new NotFoundException(errMsg);
         }
 
-        return Mono.just(new Product(productId, "Fallback product" + productId, productId, serviceUtil.getServiceAddress()));
+        return Mono.just(
+                new Product(productId, "Fallback product" + productId, productId, serviceUtil.getServiceAddress()));
     }
 
     @Override
     public Mono<Void> deleteProduct(int productId) {
 
         return Mono.fromRunnable(() -> sendMessage("products-out-0", new Event(DELETE, productId, null)))
-                   .subscribeOn(publishEventScheduler).then();
+                   .subscribeOn(publishEventScheduler)
+                   .then();
     }
 
     @Override
@@ -128,19 +142,26 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
     @Override
     public Flux<Recommendation> getRecommendations(int productId) {
 
-        URI url = UriComponentsBuilder.fromUriString(RECOMMENDATION_SERVICE_URL + "/recommendation?productId={productId}").build(productId);
+        URI url = UriComponentsBuilder.fromUriString(
+                RECOMMENDATION_SERVICE_URL + "/recommendation?productId={productId}").build(productId);
 
         LOG.debug("Will call the getRecommendations API on URL: {}", url);
 
         // Return an empty result if something goes wrong to make it possible for the composite service to return partial responses
-        return webClient.get().uri(url).retrieve().bodyToFlux(Recommendation.class).log(LOG.getName(), FINE).onErrorResume(error -> empty());
+        return webClient.get()
+                        .uri(url)
+                        .retrieve()
+                        .bodyToFlux(Recommendation.class)
+                        .log(LOG.getName(), FINE)
+                        .onErrorResume(error -> empty());
     }
 
     @Override
     public Mono<Void> deleteRecommendations(int productId) {
 
         return Mono.fromRunnable(() -> sendMessage("recommendations-out-0", new Event(DELETE, productId, null)))
-                   .subscribeOn(publishEventScheduler).then();
+                   .subscribeOn(publishEventScheduler)
+                   .then();
     }
 
     @Override
@@ -155,26 +176,31 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
     @Override
     public Flux<Review> getReviews(int productId) {
 
-        URI url = UriComponentsBuilder.fromUriString(REVIEW_SERVICE_URL + "/review?productId={productId}").build(productId);
+        URI url = UriComponentsBuilder.fromUriString(REVIEW_SERVICE_URL + "/review?productId={productId}")
+                                      .build(productId);
 
         LOG.debug("Will call the getReviews API on URL: {}", url);
 
         // Return an empty result if something goes wrong to make it possible for the composite service to return partial responses
-        return webClient.get().uri(url).retrieve().bodyToFlux(Review.class).log(LOG.getName(), FINE).onErrorResume(error -> empty());
+        return webClient.get()
+                        .uri(url)
+                        .retrieve()
+                        .bodyToFlux(Review.class)
+                        .log(LOG.getName(), FINE)
+                        .onErrorResume(error -> empty());
     }
 
     @Override
     public Mono<Void> deleteReviews(int productId) {
 
         return Mono.fromRunnable(() -> sendMessage("reviews-out-0", new Event(DELETE, productId, null)))
-                   .subscribeOn(publishEventScheduler).then();
+                   .subscribeOn(publishEventScheduler)
+                   .then();
     }
 
     private void sendMessage(String bindingName, Event event) {
         LOG.debug("Sending a {} message to {}", event.getEventType(), bindingName);
-        Message message = MessageBuilder.withPayload(event)
-                                        .setHeader("partitionKey", event.getKey())
-                                        .build();
+        Message message = MessageBuilder.withPayload(event).setHeader("partitionKey", event.getKey()).build();
         streamBridge.send(bindingName, message);
     }
 
@@ -185,7 +211,7 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
             return ex;
         }
 
-        WebClientResponseException wcre = (WebClientResponseException)ex;
+        WebClientResponseException wcre = (WebClientResponseException) ex;
 
         switch (HttpStatus.resolve(wcre.getStatusCode().value())) {
 
